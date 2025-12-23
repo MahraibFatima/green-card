c61ae24d96d28ce22b59314bc15b27f8fb8f8917
MahraibFatima <mahraibfatima5@gmail.com>
2025-12-23 23:35:01 +0500

    feat: add route for ProductDetails page to display individual product information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./components/Login";
 import { useAppContext } from "./context/AppContext";
 import AllProducts from "./pages/AllProducts";
 import ProductCategory from "./pages/ProductCategory";
-
+import ProductDetails from "./pages/ProductDetails";
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
   const { showUserLogin } = useAppContext();
@@ -27,7 +27,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<AllProducts />} />
         <Route path="/products/:category" element={<ProductCategory />} />
-
+        <Route path="/products/:category/:id" element={<ProductDetails />} />
         
         </Routes>
         
