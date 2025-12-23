9d32640231e4ebd19f45646eccc38dd90b800417
MahraibFatima <mahraibfatima5@gmail.com>
2025-12-23 23:06:48 +0500

    style: format code for consistency and readability in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,31 +3,34 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { useLocation } from "react-router-dom";
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import { useAppContext } from "./context/AppContext";
 import AllProducts from "./pages/AllProducts";
-
+import ProductCategory from "./pages/ProductCategory";
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin}=useAppContext();
+  const { showUserLogin } = useAppContext();
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
-      {showUserLogin ? <Login />: null}
+      {showUserLogin ? <Login /> : null}
 
-      <Toaster/>
-      <div  className={`${isSellerPath ? '': "px-6 md:px-16 lg:px-24 xl:px-32"} `}>
-
+      <Toaster />
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"} `}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<AllProducts />} />
+        <Route path="/products/:category" element={<ProductCategory />} />
+
+        
         </Routes>
-                <Routes>
-          <Route path="/products" element={<AllProducts />} />
-        </Routes>
+        
       </div>
       {!isSellerPath && <Footer />}
     </div>
