3ec3457c1dee5ebbade326957cc02069524a2290
MahraibFatima <mahraibfatima5@gmail.com>
2026-02-14 01:11:44 +0500

    Add protected routes for profile, login, tracking, refund, add-address, my-orders, and payment methods

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,11 @@ import ProductList from "./pages/Seller/ProductList";
 import Loading from "./components/Loading";
 import Profile from "./pages/Profile";
 import Faqs from "./components/Faqs";
+import Refund from "./pages/Refund";
+import Tracking from "./pages/Tracking";
+import PaymentMethods from "./pages/PaymentMethods";
+import { ProtectedRoute, PublicRoute, SellerProtectedRoute } from "./protectedRoutes";
+
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
   const { showUserLogin, isSeller } = useAppContext();
@@ -35,18 +40,22 @@ function App() {
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"} `}
       >
         <Routes>
+
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
+          <Route path="/login" element={<PublicRoute element={<Login />} />} />
           <Route path="/products" element={<AllProducts />} />
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/faqs" element={<Faqs />} />
-          <Route path="/add-address" element={<AddAddress />} />
-          <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/tracking" element={<ProtectedRoute element={<Tracking />} />} />
+          <Route path="/refund" element={<ProtectedRoute element={<Refund />} />} />
+          <Route path="/add-address" element={<ProtectedRoute element={<AddAddress />} />} />
+          <Route path="/my-orders" element={<ProtectedRoute element={<MyOrders />} />} />
+          <Route path="/payment-methods" element={<ProtectedRoute element={<PaymentMethods />} />} />
           <Route path="/loader" element={<Loading />} />
-          <Route path="/seller"element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+          <Route path="/seller" element={<SellerProtectedRoute element={isSeller ? <SellerLayout /> : <SellerLogin />} isSeller={isSeller} />}>
             <Route index element={isSeller ? <AddProduct /> : null} />
             <Route path="product-list" element={<ProductList />} />
             <Route path="orders" element={<Orders />} />
