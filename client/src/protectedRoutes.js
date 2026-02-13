32ab27799f0a048397f7f04d0640806092c98d89
MahraibFatima <mahraibfatima5@gmail.com>
2026-02-14 01:15:27 +0500

    Add protected route components for user and seller navigation

---
+++ client/src/protectedRoutes.js
@@ -0,0 +1,25 @@
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from './context/AppContext';
+
+
+export const ProtectedRoute = ({ element }) => {
+  const { user } = useAppContext();
+  
+  return user ? element : <Navigate to="/login" replace />;
+};
+
+export const PublicRoute = ({ element }) => {
+  const { user } = useAppContext();
+  
+  return user ? <Navigate to="/" replace /> : element;
+};
+
+export const SellerProtectedRoute = ({ element, isSeller }) => {
+  const { user } = useAppContext();
+  
+  if (!user) {
+    return <Navigate to="/seller" replace />;
+  }
+  
+  return isSeller ? element : <Navigate to="/seller" replace />;
+};
